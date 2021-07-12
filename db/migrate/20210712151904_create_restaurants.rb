4bb569c5c2af607b531888cacb4f6f84d855c800
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-07-12 16:17:24 +0200

    wip

---
+++ db/migrate/20210712151904_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.float  :rating
+
+      t.timestamps # add 2 columns, `created_at` and `updated_at`
+    end
+  end
+end
